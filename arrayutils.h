39606a7101d2aa5ff74d567f59f1718ac492e1a3
lucasfrison <64698867+lucasfrison@users.noreply.github.com>
2022-07-15 08:35:26 -0300

    Update arrayutils.h

--- arrayutils.h
+++ arrayutils.h
@@ -1,11 +1,19 @@
 #ifndef ARRAYUTILS_H
 #define ARRAYUTILS_H
 
+    //propriedade de vetor, retorna o tamanho inteiro do vetor
     #define length(arr) sizeof(arr) / sizeof(arr[0])
     
+    //forEach para listas, NÃO TESTADO
     #define forEachItem(item, list) \
     for(T * item = list->head; item != NULL; item = item->next)
-            
+      
+    /*Diretiva forEach
+    Sintaxe: 
+    forEach(item, array) {
+        instruções;
+        instruções;
+    } */
     #define forEach(item, array) \
     for(int keep = 1, \
     count = 0,\
@@ -14,7 +22,7 @@
     keep = !keep, count++) \
     for(item = (array) + count; keep; keep = !keep)
     
-    //search for a value inside an array, return the position number       
+    //algoritmos de pesquisa       
     extern int searchInt(int*, int, int);
     extern float searchFloat(float*, int, float);
     extern double searchDouble(double*, int, double);
